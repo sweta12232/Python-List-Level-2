7a6e98eb848ee7d65825e9ed71431efc4061efc3
sweta12232 <131160939+sweta12232@users.noreply.github.com>
2024-04-27 19:54:10 +0545

    Create list12.py[remove, del, clear, pop function]

---
+++ list12.py
@@ -0,0 +1,20 @@
+list = ["Sweta", 11, "Bishnu", 22, "Suraj", 33, "Swopnil"]
+list.remove(11)
+print(list)
+
+list = ["Sweta", 11, "Bishnu", 22, "Suraj", 33, "Swopnil"]
+list.pop() # this remove last items , we can also use pop(-1)
+print(list)
+
+
+list = ["Sweta", 11, "Bishnu", 22, "Suraj", 33, "Swopnil"]
+del list[2]
+print(list)
+
+list = ["Sweta", 11, "Bishnu", 22, "Suraj", 33, "Swopnil"]
+del list
+print(list)
+
+list = ["Sweta", 11, "Bishnu", 22, "Suraj", 33, "Swopnil"]
+list.clear()
+print(list)
