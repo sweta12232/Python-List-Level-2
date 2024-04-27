2005f8b3a90ccd3d8a4c1b47376e97ca22209251
sweta12232 <131160939+sweta12232@users.noreply.github.com>
2024-04-27 19:39:29 +0545

    Update list10.py

--- list10.py
+++ list10.py
@@ -1,3 +1,11 @@
+# sort give result in ascending by default
+
 list = ["Nunu", "Ram", "Sita", "Shyam", "Radha", "Mohan"]
 list.sort()
+print(list)
+
+#sore(reverse = True), this function give descending
+
+list = ["Nunu", "Ram", "Sita", "Shyam", "Radha", "Mohan"]
+list.sort(reverse = True)
 print(list)
