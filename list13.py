d8c22b838bfb2e6a4cd4df3f1efca222f9c0939c
sweta12232 <131160939+sweta12232@users.noreply.github.com>
2024-04-27 19:59:04 +0545

    Create list13.py[count function]

---
+++ list13.py
@@ -0,0 +1,5 @@
+# The count() method in Python is used to count the number of occurrences of a specific element in a list
+
+list = ["nunu", "bunu", "nunu", "sita", "nunu", "ram"]
+counting = list.count("nunu")
+print(f'The number of nunu is {counting}')
