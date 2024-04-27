c4b5dc77e20c7da8a2320b1f367771027e7b0c4f
sweta12232 <131160939+sweta12232@users.noreply.github.com>
2024-04-27 19:26:11 +0545

    Create list9.py[extend function]

---
+++ list9.py
@@ -0,0 +1,9 @@
+list = ["Sweta", 11, "Bishnu", 22, "Suraj", 33, "Swopnil"]
+lis= ["Sunil", "Gita"]
+lis.extend(list)
+print(lis)
+
+list = ["Sweta", 11, "Bishnu", 22, "Suraj", 33, "Swopnil"]
+lis= ["Sunil", "Gita"]
+list.extend(lis)
+print(list)
