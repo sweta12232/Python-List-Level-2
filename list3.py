b398c0d0feb21d15765830c92d5239c7d7d9b0e2
sweta12232 <131160939+sweta12232@users.noreply.github.com>
2024-04-30 23:42:55 +0545

    Add files via upload

--- list3.py
+++ list3.py
@@ -1,3 +1,5 @@
-list = ["sweta", "bishnu", "suraj", "suraj", "suraj"]
-print(len(list))
-#Len function used to word total list. For example sweta = 1
+salary = [10000, 12000,20000, 15000,40000,5000]
+#increase the all salary by 5% whoose salary is less than 15000
+
+newSalary= [  x + 0.05*x  for x in salary if x < 15000]
+print(newSalary)
